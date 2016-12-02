c5e6120801ec80a41cfa50bc710578a95b5dcffa
Kotaru <110190@CTS.COM>
2016-12-02 12:43:36 +0530

    Added code comments to Flow basic sample

--- Flow-basic-sample/index.js
+++ Flow-basic-sample/index.js
@@ -1,12 +1,13 @@
 // @flow
-var message = "Hello Flow";
-console.log(message);
 
+// Flow implies first parameter is a string. It shows error as arithmetic operation is performed on string 
 console.log(((number1, number2) => number1 * number2)('Im String',20));
 
-let isItCool:boolean;
-isItCool = "test";
+let isItCool:boolean; // declare a variable
+isItCool = "test"; // Shows error as string is attempted to be assigned to a boolean type
 
+// Types are defined on parameters, making developer intent clear.
 console.log(((number1: number, number2: number) => number1 * number2)('Im String',20));
 
+// defining return type checks return value.
 ((number1, number2):number => number1 + number2)('10',20);
